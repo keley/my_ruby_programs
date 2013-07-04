ec4149e46c1b8ef372f0cccd7f87296f3873343a
Ken Eley <keley@internode.on.net>
2013-07-04 16:36:52 +1000

    added comment.  note also that this takes us to day 6, not day 5.

--- simple_rack.rb
+++ simple_rack.rb
@@ -3,3 +3,6 @@
 arg = ARGV.first
 rack_echo = lambda { |env| [200, { 'Content-Type' => "text/plain"}, ["Command line argument you typed was: #{arg}"] ] }
 puts rack_echo.call({})
+
+# TODO: this is rather inelegant in format.  Also, it would be nice to pass
+# arg as an argument rather than simply interpolating into the lambda
